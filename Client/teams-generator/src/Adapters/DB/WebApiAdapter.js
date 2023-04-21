122ae15071e7882b75877f2350cf6b71c11636dd
eladpeleg <potokelad@gmail.com>
2023-04-21 23:10:11 +0300

    change url

--- Client/teams-generator/src/Adapters/DB/WebApiAdapter.js
+++ Client/teams-generator/src/Adapters/DB/WebApiAdapter.js
@@ -3,13 +3,13 @@ const realService = 'https://teamsgeneratorwebapi20230420202750.azurewebsites.ne
 const localEnv = "https://localhost:7236"
 
 export async function getInitialConfig() {
-    const resposne = await fetch(`${localEnv}/Home`)
+    const resposne = await fetch(`${realService}/Home`)
     return resposne.json()
 }
 
 export async function getTeams(players, config){
     console.log('config',config)
-    const resposne = await fetch(`${localEnv}/Teams?algoKey=${config.algo.algoKey}`, {
+    const resposne = await fetch(`${realService}/Teams?algoKey=${config.algo.algoKey}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ config: config, players: players}),
@@ -19,7 +19,7 @@ export async function getTeams(players, config){
 
 
 export async function getResultString(teams){
-    const resposne = await fetch(`${localEnv}/Teams/PostResultString`, {
+    const resposne = await fetch(`${realService}/Teams/PostResultString`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ teams}),
